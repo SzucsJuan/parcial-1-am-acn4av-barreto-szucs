f0b01668c949f1fc0ef28e42a65f6c2e3589b799
IanBarreto332 <ianbarreto99@gmail.com>
2024-09-28 00:39:31 -0300

    feat(action) add onClick event to aproval and reject buttons

--- app/src/main/java/com/example/parcial_1_am_acn4av_barreto_szucs/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_barreto_szucs/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.parcial_1_am_acn4av_barreto_szucs;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button aproval_button, reject_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void AprovalBtn(View view){
+        Toast.makeText(getApplicationContext(),"Bien maquina, ahora calma la fiera, yo te aviso si esta para hablarle", Toast.LENGTH_SHORT).show();
+    }
+
+    public void RejectBtn(View view){
+        Toast.makeText(getApplicationContext(),"Que mal, yo pense que era tu tipo", Toast.LENGTH_SHORT).show();
     }
 }
