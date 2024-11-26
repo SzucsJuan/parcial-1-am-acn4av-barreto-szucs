ef57ecba57b82c70adff292d6189783541ca7901
IanBarreto332 <ianbarreto99@gmail.com>
2024-11-26 11:46:59 -0300

    feat(UI) Add ProfileActivity with own style and methods

--- app/src/main/java/com/example/parcial_1_am_acn4av_barreto_szucs/ProfileActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_barreto_szucs/ProfileActivity.java
@@ -2,10 +2,8 @@ package com.example.parcial_1_am_acn4av_barreto_szucs;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +13,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
+    Button logout_button, home_button;
     FirebaseAuth mAuth;
 
     @Override
@@ -24,29 +23,28 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-    }
+        logout_button = findViewById(R.id.logoutBtn);
 
-    public void ProfileBtn(View view){
-        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-        startActivity(intent);
-    }
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+            }
+            public void HomeBtn(View view){
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    public void AprovalBtn(View view){
-        Intent intent = new Intent(MainActivity.this, AprovalActivity.class);
-        startActivity(intent);
     }
-
-    public void RejectBtn(View view){
-        Intent intent = new Intent(MainActivity.this, RejectActivity.class);
-        startActivity(intent);
-    }
-
 
 }
